2bfe7a38ce65d6aa15c1d2abc9f951282c069678
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-11 17:04:21 -0700

    Initial Commit

---
+++ SVNModalViewController/SVNModalViewController.h
@@ -0,0 +1,19 @@
+//
+//  SVNModalViewController.h
+//  SVNModalViewController
+//
+//  Created by Aaron Dean Bikis on 4/11/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNModalViewController.
+FOUNDATION_EXPORT double SVNModalViewControllerVersionNumber;
+
+//! Project version string for SVNModalViewController.
+FOUNDATION_EXPORT const unsigned char SVNModalViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNModalViewController/PublicHeader.h>
+
+
