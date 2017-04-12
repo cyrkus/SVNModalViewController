b3334ea18a9ac5c27994aa5f0005c63a54244874
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-11 17:08:28 -0700

    create xcode proj and add dependencies

---
+++ SVNModalViewController/SVNModalViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SVNModalViewController.swift
+//  Tester
+//
+//  Created by Aaron Dean Bikis on 4/11/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+import UIKit
+import SVNTheme
+import SVNShapesManager
+
+
+public class SVNModalViewController: UIViewController {
+    
+    private lazy var dismissMeta: SVNShapeMetaData = {
+        let shape = SVNShapeMetaData(shapes: nil,
+                                     location: .topLeft,
+                                     padding: CGPoint(x: 10.0, y: 15.0),
+                                     size: CGSize(width: 65.0, height: 65.0),
+                                     fill: UIColor.clear.cgColor,
+                                     stroke: self.theme.stanardButtonTintColor.cgColor,
+                                     strokeWidth: 2.5)
+        return shape
+    }()
+    
+    public lazy var dismissButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(SVNModalViewController.shouldDismiss), for: .touchUpInside)
+        self.view.addSubview(button)
+        return button
+    }()
+    
+    public lazy var shapesManager: SVNShapesManager = {
+        let manager = SVNShapesManager(container: self.view.bounds)
+        return manager
+    }()
+    
+    public var theme = SVNTheme_DefaultDark()
+    
+    /**
+     Call this in the viewdidload of whatever subclass is conforming after the other layers have been set
+    */
+    public func addModalSubviews(){
+        self.dismissButton.frame = self.shapesManager.fetchRect(for: .topLeft, with: CGPoint(x: 0, y:0), and: CGSize(width: 85, height: 85))
+        self.dismissMeta.shapes = self.shapesManager.createTwoLines(with: dismissMeta, shapeToCreate: .exit)
+        self.dismissMeta.shapes?.forEach({ self.view.layer.addSublayer($0) })
+    }
+    
+    public func shouldDismiss(){
+        self.dismiss(animated: true, completion: nil)
+    }
+}
