31aac69e4c88815f0ad63760153241fad9a4c814
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-27 17:18:22 -0700

    allow hiding of modal subviews

--- SVNModalViewController/SVNModalViewController.swift
+++ SVNModalViewController/SVNModalViewController.swift
@@ -12,43 +12,51 @@ import SVNShapesManager
 
 /**
  A modal viewController to be subclassed. Contains a Top Left dismissButton and uses *SVNShapesManager* & *SVNTheme*
-    To add the button on the top left call addModalSubviews in viewDidLoad
-    Be sure that this viewController is presented modally otherwise override *shouldDismiss*
+ To add the button on the top left call addModalSubviews in viewDidLoad
+ Be sure that this viewController is presented modally otherwise override *shouldDismiss*
  */
 open class SVNModalViewController: UIViewController {
-    
-    private lazy var dismissMeta: SVNShapeMetaData = {
-        let shape = SVNShapeMetaData(shapes: nil,
-                                     location: .topLeft,
-                                     padding: CGPoint(x: 10.0, y: 15.0),
-                                     size: CGSize(width: 65.0, height: 65.0),
-                                     fill: UIColor.clear.cgColor,
-                                     stroke: self.theme.stanardButtonTintColor.cgColor,
-                                     strokeWidth: 2.5)
-        return shape
-    }()
-    
-    public lazy var dismissButton: UIButton = {
-        let button = UIButton()
-        button.addTarget(self, action: #selector(SVNModalViewController.shouldDismiss), for: .touchUpInside)
-        self.view.addSubview(button)
-        return button
-    }()
-    
-    open lazy var shapesManager: SVNShapesManager = {
-        let manager = SVNShapesManager(container: self.view.bounds)
-        return manager
-    }()
-    
-    open var theme: SVNTheme = SVNTheme_DefaultDark()
-    
-    public func addModalSubviews(){
-        self.dismissButton.frame = self.shapesManager.fetchRect(for: .topLeft, with: CGPoint(x: 0, y:0), and: CGSize(width: 85, height: 85))
-        self.dismissMeta.shapes = self.shapesManager.createTwoLines(with: dismissMeta, shapeToCreate: .exit)
-        self.dismissMeta.shapes?.forEach({ self.view.layer.addSublayer($0) })
-    }
-    
-    open func shouldDismiss(){
-        self.dismiss(animated: true, completion: nil)
-    }
+  
+  public lazy var dismissMeta: SVNShapeMetaData = {
+    let shape = SVNShapeMetaData(shapes: nil,
+                                 location: .topLeft,
+                                 padding: CGPoint(x: 10.0, y: 15.0),
+                                 size: CGSize(width: 65.0, height: 65.0),
+                                 fill: UIColor.clear.cgColor,
+                                 stroke: self.theme.stanardButtonTintColor.cgColor,
+                                 strokeWidth: 2.5)
+    return shape
+  }()
+  
+  public lazy var dismissButton: UIButton = {
+    let button = UIButton()
+    button.addTarget(self, action: #selector(SVNModalViewController.shouldDismiss), for: .touchUpInside)
+    self.view.addSubview(button)
+    return button
+  }()
+  
+  open lazy var shapesManager: SVNShapesManager = {
+    let manager = SVNShapesManager(container: self.view.bounds)
+    return manager
+  }()
+  
+  open var theme: SVNTheme = SVNTheme_DefaultDark()
+  
+  
+  public func addModalSubviews(){
+    self.dismissButton.frame = self.shapesManager.fetchRect(for: .topLeft, with: CGPoint(x: 0, y:0), and: CGSize(width: 85, height: 85))
+    self.dismissMeta.shapes = self.shapesManager.createTwoLines(with: dismissMeta, shapeToCreate: .exit)
+    self.dismissMeta.shapes?.forEach({ self.view.layer.addSublayer($0) })
+  }
+  
+  
+  public func modalSubviews(shouldHide hide: Bool){
+    dismissButton.isEnabled = hide
+    dismissMeta.shapes?.forEach({ $0.opacity = hide ? 0.0 : 1.0 })
+  }
+  
+  
+  open func shouldDismiss(){
+    self.dismiss(animated: true, completion: nil)
+  }
 }
